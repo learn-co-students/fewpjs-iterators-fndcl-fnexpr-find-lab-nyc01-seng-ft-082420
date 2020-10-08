01e88a830b5c59c9e44165bf1868f68b6cc736e0
AB <austinbuhler@gmail.com>
2020-10-08 18:38:09 -0400

    Cleaner

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function testFunc() {
 }
 
 const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
+  {year: "2018", result: "W"},
+  {year: "2017", result: "L"},
   {year: "2016", result: "N/A"}
   //...
 ]
 
 const superbowlWin = recordsArray => {
   let results = recordsArray.find( record => record.result === "W" )
-  return results.year
+  return !!results ? results.year : undefined
 }
