edc6d8954f0fb60d2f3973584177050781127907
Eddy_Yunhao_Gao <as510891328@gmail.com>
2020-10-08 22:24:25 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let wins = array.find(win => win.result === 'W')
+  if(typeof wins != 'undefined'){
+    return wins.year
+  }else{
+    return undefined
+  }
+}
