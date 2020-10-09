c43d989c12ef6357030fe0f71156e093b1f24432
ashabahmed <ashabahmed7@gmail.com>
2020-10-09 08:44:02 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(r => r.result === "W")
+    if (!!win === true) {
+      return win.year
+    } else {
+      return undefined
+    }
+}
