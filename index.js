b64aefd94fbfcdd040712f0a16a48f707624863a
jakeFromm <jacobfromm@gmail.com>
2020-10-17 19:29:25 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (statsArray) => {
+  let result = statsArray.find(stat => stat.result === "W")
+    if (result){
+      return result.year
+    } else {
+      return undefined
+    }
+}
